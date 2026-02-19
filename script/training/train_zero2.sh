6cddeca59a7572f2d60e5b5c86237ca05b25ec16
alphabet <1576012404@qq.com>
2026-02-19 16:36:50 +0800

    update

--- script/training/train_zero2.sh
+++ script/training/train_zero2.sh
@@ -2,12 +2,15 @@
 
 set -ex
 
-GPUs="0,1,2,3,4,5,6,7"
+GPUs="0,1"
 
 # export HF_ENDPOINT=https://hf-mirror.com
 export CUDA_VISIBLE_DEVICES=$GPUs
 export NCCL_P2P_LEVEL=NVL
 export CUDA_LAUNCH_BLOCKING=1
+export EVAL_LLM_BASE_URL="https://dashscope.aliyuncs.com/compatible-mode/v1"
+export EVAL_LLM_API_KEY="sk-d06d58564b4e42e99c0b8e2566289867"
+export EVAL_LLM_MODEL_NAME="qwen-plus"
 
 number_of_gpus=$(echo $GPUs | tr ',' '\n' | wc -l)
 
--- script/training/train_zero3.sh
+++ script/training/train_zero3.sh
@@ -7,9 +7,13 @@ export NCCL_P2P_DISABLE=0
 export NCCL_P2P_LEVEL=NVL
 export CUDA_LAUNCH_BLOCKING=1
 
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch \
+export EVAL_LLM_BASE_URL="https://dashscope.aliyuncs.com/compatible-mode/v1"
+export EVAL_LLM_API_KEY="sk-d06d58564b4e42e99c0b8e2566289867"
+export EVAL_LLM_MODEL_NAME="qwen-plus"
+
+accelerate launch \
     --config_file ./src/config/accelerate_config/train_zero3.yaml \
     --main_process_port 12347 \
-    --num_processes 4 \
+    --num_processes 2 \
     --mixed_precision "fp16" \
     ./src/train.py
